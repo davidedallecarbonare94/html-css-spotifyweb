b30c6a4bb621f605c2453506fbf41309672ea113
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-04-21 14:42:57 +0200

    add js to page

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/* Write Line */
+
+
+
+
+/* Alert */
+alert("Attenzione! Sembra che la tua sia una versione di Spotify non conforme all'originale!");
+
+
+/* Get by element */
+document.getElementById("change_me").innerHTML = "Guarda come cambio!"
+
+document.getElementById("also_me_pls").innerHTML = "Cambio anche io con JS"
